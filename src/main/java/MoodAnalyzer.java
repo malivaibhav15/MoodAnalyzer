5022d79f73adb311eda7c9ee93b7ffc6b7a4b4c8
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-06 18:01:42 +0530

    ADDED : Display mood through constructor

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,14 +1,22 @@
 public class MoodAnalyzer
 {
-    public static void main(String[] args) {
+    private String message;
+    public static void main(String[] args)
+    {
         System.out.println("Welcome to mood analyzer problem");
     }
-    public String analyseMood(String message)
+    public String analyseMood()
     {
-        if(message.contains("Sad"))
+        if (message.contains("Sad"))
             return "Sad";
         else
             return "Happy";
     }
-
+    public MoodAnalyzer()
+    {
+    }
+    public MoodAnalyzer(String message)
+    {
+        this.message = message;
+    }
 }
--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -3,17 +3,16 @@ import org.junit.Test;
 
 public class TestCases
 {
-    MoodAnalyzer MoodAnalyser = new MoodAnalyzer();
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad()
     {
-        String mood=MoodAnalyser.analyseMood("I am in Sad Mood");
-        Assert.assertEquals("Sad",mood);
+        MoodAnalyzer MoodAnalyser=new MoodAnalyzer("I am in Sad Mood");
+        Assert.assertEquals("Sad",MoodAnalyser.analyseMood());
     }
     @Test
     public void givenMessage_WhenIncludeIAmInAnyMood_ShouldReturnHappy()
     {
-        String mood=MoodAnalyser.analyseMood(("I am In Any Mood"));
-        Assert.assertEquals("Happy",mood);
+        MoodAnalyzer MoodAnalyser=new MoodAnalyzer("I am in Any Mood");
+        Assert.assertEquals("Happy",MoodAnalyser.analyseMood());
     }
 }
