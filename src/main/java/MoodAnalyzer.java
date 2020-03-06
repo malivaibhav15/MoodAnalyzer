0ad9e5b76adf0cb13d635d36e2243569df402913
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-06 17:10:57 +0530

    ADDED: Display mood

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -3,5 +3,12 @@ public class MoodAnalyzer
     public static void main(String[] args) {
         System.out.println("Welcome to mood analyzer problem");
     }
+    public String analyseMood(String message)
+    {
+        if(message.contains("Sad"))
+            return "Sad";
+        else
+            return "Happy";
+    }
 
 }
--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,2 +1,19 @@
-public class TestCases {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCases
+{
+    MoodAnalyzer MoodAnalyser = new MoodAnalyzer();
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad()
+    {
+        String mood=MoodAnalyser.analyseMood("I am in Sad Mood");
+        Assert.assertEquals("Sad",mood);
+    }
+    @Test
+    public void givenMessage_WhenIncludeIAmInAnyMood_ShouldReturnHappy()
+    {
+        String mood=MoodAnalyser.analyseMood(("I am In Any Mood"));
+        Assert.assertEquals("Happy",mood);
+    }
 }
