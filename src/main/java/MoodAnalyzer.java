d27e0567dd88ee238df76e9efc17c856a18284f0
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-11 15:30:13 +0530

    ADDED : Handle null pointer exception

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -5,18 +5,21 @@ public class MoodAnalyzer
     {
         System.out.println("Welcome to mood analyzer problem");
     }
-    public String analyseMood()
-    {
-        if (message.contains("Sad"))
-            return "Sad";
-        else
-            return "Happy";
-    }
     public MoodAnalyzer()
     {
     }
     public MoodAnalyzer(String message)
     {
         this.message = message;
+    }
+    public String analyseMood() {
+        try {
+            if (message.contains("Sad"))
+                return "Sad";
+            else
+                return "Happy";
+        } catch (NullPointerException npe) {
+            return "Happy";
+        }
     }
 }
--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,3 +1,4 @@
+import com.sun.org.apache.bcel.internal.generic.PUSH;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -6,13 +7,19 @@ public class TestCases
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad()
     {
-        MoodAnalyzer MoodAnalyser=new MoodAnalyzer("I am in Sad Mood");
+        MoodAnalyzer MoodAnalyser = new MoodAnalyzer("I am in Sad Mood");
         Assert.assertEquals("Sad",MoodAnalyser.analyseMood());
     }
     @Test
     public void givenMessage_WhenIncludeIAmInAnyMood_ShouldReturnHappy()
     {
-        MoodAnalyzer MoodAnalyser=new MoodAnalyzer("I am in Any Mood");
+        MoodAnalyzer MoodAnalyser = new MoodAnalyzer("I am in Any Mood");
+        Assert.assertEquals("Happy",MoodAnalyser.analyseMood());
+    }
+    @Test
+    public void  GivenMessage_WhenNull_ShouldThrowExcepton()
+    {
+        MoodAnalyzer MoodAnalyser = new MoodAnalyzer(null);
         Assert.assertEquals("Happy",MoodAnalyser.analyseMood());
     }
 }
