007b566b6c775ab4ce7a89356f9f6f082f08be7d
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-12 13:05:17 +0530

    ADDED : Check message given is empty or not

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -5,21 +5,40 @@ import org.junit.Test;
 public class TestCases
 {
     @Test
-    public void givenMessage_WhenSad_ShouldReturnSad()
-    {
+    public void givenMessage_WhenSad_ShouldReturnSad() throws MoodAnalysisException {
         MoodAnalyzer MoodAnalyser = new MoodAnalyzer("I am in Sad Mood");
         Assert.assertEquals("Sad",MoodAnalyser.analyseMood());
     }
     @Test
-    public void givenMessage_WhenIncludeIAmInAnyMood_ShouldReturnHappy()
-    {
+    public void givenMessage_WhenIncludeIAmInAnyMood_ShouldReturnHappy() throws MoodAnalysisException {
         MoodAnalyzer MoodAnalyser = new MoodAnalyzer("I am in Any Mood");
         Assert.assertEquals("Happy",MoodAnalyser.analyseMood());
     }
     @Test
-    public void  GivenMessage_WhenNull_ShouldThrowExcepton()
-    {
+    public void  GivenMessage_WhenNull_ShouldThrowExcepton() throws MoodAnalysisException {
         MoodAnalyzer MoodAnalyser = new MoodAnalyzer(null);
         Assert.assertEquals("Happy",MoodAnalyser.analyseMood());
+    }
+    @Test
+    public void givenMessage_WhenNull_ThenShouldReturnCustomException() {
+        try {
+            MoodAnalyzer MoodAnalyser = new MoodAnalyzer(null);
+            MoodAnalyser.analyseMood();
+        }
+        catch(MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.UserDefinedDataType.NULL_EXCEPTION,e.userDefinedObject);
+        }
+    }
+    @Test
+    public void GivenEmptyMessage_ShouldReturn_EmptyMoodException()
+    {
+        try{
+            MoodAnalyzer MoodAnalyser = new MoodAnalyzer("");
+            MoodAnalyser.analyseMood();
+        }
+        catch (MoodAnalysisException e)
+        {
+            Assert.assertEquals(MoodAnalysisException.UserDefinedDataType.EMPTY_EXCEPTION,e.userDefinedObject);
+        }
     }
 }
