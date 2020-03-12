dea905672254717ee2fc602f06d739e6c5de417c
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-12 12:56:58 +0530

    ADDED : Enum class with constructor

--- src/main/java/MoodAnalysisException.java
+++ src/main/java/MoodAnalysisException.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
-public class MoodAnalysisException {
+public class MoodAnalysisException extends Exception
+{
+    enum UserDefinedDataType
+    {
+        NULL_EXCEPTION,
+        EMPTY_EXCEPTION
+    };
+    UserDefinedDataType userDefinedObject;
+    public MoodAnalysisException(String message, UserDefinedDataType userDefinedObject)
+    {
+        super(message);
+        this.userDefinedObject = userDefinedObject;
+    }
 }
