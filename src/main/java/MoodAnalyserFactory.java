bead2d73200742beb9e2eab9c6483b1352d89b94
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-13 18:05:48 +0530

    ADDED : Invoke method

--- src/main/java/MoodAnalyserFactory.java
+++ src/main/java/MoodAnalyserFactory.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MoodAnalyserFactory
 {
@@ -38,4 +39,20 @@ public class MoodAnalyserFactory
         }
         return null;
     }
+    public static Object invokeMethod(Object moodAnalyserObject, String analyzer) {
+        try {
+            Class objectClass = moodAnalyserObject.getClass();
+            Method moodMethod = objectClass.getMethod(analyzer);
+            Object result = moodMethod.invoke(moodAnalyserObject);
+            return result;
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
