f00da8ba3caf62a6abc750c05b880f8a1e6eb9d1
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-13 15:49:06 +0530

    ADDED : Throws exception when objects are different

--- src/main/java/MoodAnalysisException.java
+++ src/main/java/MoodAnalysisException.java
@@ -2,10 +2,7 @@ public class MoodAnalysisException extends Exception
 {
     enum UserDefinedDataType
     {
-        NULL_EXCEPTION,
-        EMPTY_EXCEPTION,
-        NO_SUCH_CLASS,
-        NO_SUCH_METHOD
+        NULL_EXCEPTION,EMPTY_EXCEPTION,NO_SUCH_CLASS,NO_SUCH_METHOD
     };
     UserDefinedDataType userDefinedObject;
     public MoodAnalysisException(String message, UserDefinedDataType userDefinedObject)
