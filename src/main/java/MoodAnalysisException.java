581871c824c0fb0858b6fd836cb631b679b5e4d4
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-14 15:44:47 +0530

    ADDED : Null message with reflector gives exception

--- src/main/java/MoodAnalysisException.java
+++ src/main/java/MoodAnalysisException.java
@@ -2,7 +2,7 @@ public class MoodAnalysisException extends Exception
 {
     enum UserDefinedDataType
     {
-        NULL_EXCEPTION,EMPTY_EXCEPTION,NO_SUCH_CLASS,NO_SUCH_METHOD
+        NULL_EXCEPTION,EMPTY_EXCEPTION,NO_SUCH_CLASS,NO_SUCH_METHOD, NO_SUCH_FIELD, NO_ACCESS, METHOD_INVOCATION_ISSUE
     };
     UserDefinedDataType userDefinedObject;
     public MoodAnalysisException(String message, UserDefinedDataType userDefinedObject)
