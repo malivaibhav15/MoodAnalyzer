89d8ea0215e94f8c6cb572c9401dd420e4a64676
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-14 14:57:06 +0530

    ADDED : Value of fields

--- src/main/java/MoodAnalyserFactory.java
+++ src/main/java/MoodAnalyserFactory.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -54,6 +55,19 @@ public class MoodAnalyserFactory
             throw new MoodAnalysisException("Method not found",MoodAnalysisException.UserDefinedDataType.NO_SUCH_METHOD);
         }
         return null;
+    }
+    public static void setFieldValue(Object moodObject, String fieldName, String fieldValue) throws MoodAnalysisException {
+        try {
+            Class className = moodObject.getClass();
+            Field field = className.getDeclaredField(fieldName);
+            field.set(moodObject,fieldValue);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+
+        }
+
     }
 
 }
